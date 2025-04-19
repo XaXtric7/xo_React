409fbe88ffb23960448658bfc680a28d0634853d
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2025-04-19 09:11:29 +0530

    React.intro

--- V_01/src/App.js
+++ V_01/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
+  const [value, setValue] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar logoText="CodeWithCWHHarry" />
+      <div className="value"> {value}</div>
+      <button
+        onClick={() => {
+          setValue(value + 1);
+        }}
+      >
+        Click me
+      </button>
+      <Footer />
     </div>
   );
 }
