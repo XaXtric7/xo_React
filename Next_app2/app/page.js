6089be14202bde98f8d8bb4c793ba8ddddbd7716
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2025-07-01 01:39:13 +0530

    Server Components & use client

--- Next_app2/app/page.js
+++ Next_app2/app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
   });
   return (
     <div>
-      <Navbar />I am a component
+      <Navbar />I am one of the component
       {/* {count} */}
       {/* <button onClick={()=> setCount(count + 1)}>Click me</button> */}
     </div>
