c0b3fe9f16fe765b2f3b8e933b525f05a2ac98ae
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2025-07-02 02:03:32 +0530

    Auth.js_Intro

---
+++ Next_app7/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+
+const handler = NextAuth({
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  debug: true, // helpful to see what's going wrong in terminal
+});
+
+export { handler as GET, handler as POST };
